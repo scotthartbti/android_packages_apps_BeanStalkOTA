AOSPA/android_packages_apps_ParanoidOTA

Version 1.6: Allow checking goo.im if pa.co fails

src/com/paranoid/paranoidota/updater/RomUpdater.java
@@ -16,11 +16,7 @@
 
 package com.paranoid.paranoidota.updater;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import android.content.Context;
 
@@ -28,14 +24,21 @@ import com.paranoid.paranoidota.R;
 import com.paranoid.paranoidota.Utils;
 import com.paranoid.paranoidota.helpers.SettingsHelper;
 import com.paranoid.paranoidota.http.URLStringReader;
+import com.paranoid.paranoidota.updater.server.GooServer;
+import com.paranoid.paranoidota.updater.server.PaServer;
 
 public class RomUpdater extends Updater {
 
-    private static final String URL = "http://api.paranoidandroid.co/updates/%s?v=%s";
+    private static final Server[] SERVERS = {
+        new PaServer(),
+        new GooServer()
+    };
 
     private SettingsHelper mSettingsHelper;
+    private Server mServer;
     private boolean mScanning = false;
     private boolean mFromAlarm;
+    private int mCurrentServer = -1;
 
     public RomUpdater(Context context, boolean fromAlarm) {
         super(context);
@@ -54,16 +57,23 @@ public class RomUpdater extends Updater {
         }
         mScanning = true;
         fireStartChecking();
-        long version = !Utils.weAreInAospa() ? 0L : getVersion();
-        new URLStringReader(this)
-                .execute(String.format(URL, new Object[] { getDevice(), version }));
+        nextServerCheck();
+    }
+
+    private void nextServerCheck() {
+        mScanning = true;
+        mCurrentServer++;
+        mServer = SERVERS[mCurrentServer];
+        new URLStringReader(this).execute(mServer.getUrl(getDevice(), getVersion()));
     }
 
     @Override
     public long getVersion() {
         String version = Utils.getProp(Utils.MOD_VERSION);
+        version = version.replaceAll(".1-RC1-", "-");
+        version = version.replaceAll("-RC2-", "-");
         String stripped = version.replaceAll("\\D+", "");
-        return Long.parseLong(stripped);
+        return "".equals(stripped) ? 0L : Long.parseLong(stripped);
     }
 
     @Override
@@ -86,22 +96,9 @@ public class RomUpdater extends Updater {
             mScanning = false;
             PackageInfo[] lastRoms = null;
             setLastUpdates(null);
-            String error = null;
-            List<PackageInfo> list = new ArrayList<PackageInfo>();
-            if (buffer != null && !buffer.isEmpty()) {
-                JSONObject updateInfo = new JSONObject(buffer);
-                error = updateInfo.optString("error");
-                if (error == null || error.isEmpty()) {
-                    JSONArray updates = updateInfo.getJSONArray("updates");
-                    for (int i = updates.length() - 1; i >= 0; i--) {
-                        JSONObject update = updates.getJSONObject(i);
-                        list.add(new UpdatePackage(getDevice(), update.getString("name"), update
-                                .getLong("version"), update.getString("size"), update
-                                .getString("url"), update.getString("md5"), false));
-                    }
-                }
-            }
+            List<PackageInfo> list = mServer.createPackageInfoList(buffer);
             lastRoms = list.toArray(new PackageInfo[list.size()]);
+            String error = mServer.getError();
             if (list.size() > 0) {
                 if (mFromAlarm) {
                     Utils.showNotification(getContext(), lastRoms, ROM_NOTIFICATION_ID, !Utils
@@ -110,13 +107,16 @@ public class RomUpdater extends Updater {
                 }
             } else {
                 if (error != null && !error.isEmpty()) {
-                    versionError(error);
+                    if (versionError(error)) {
+                        return;
+                    }
                 } else {
                     if (!mFromAlarm) {
                         Utils.showToastOnUiThread(getContext(), R.string.check_rom_updates_no_new);
                     }
                 }
             }
+            mCurrentServer = -1;
             setLastUpdates(lastRoms);
             fireCheckCompleted(lastRoms);
         } catch (Exception ex) {
@@ -132,16 +132,23 @@ public class RomUpdater extends Updater {
         versionError(null);
     }
 
-    private void versionError(String error) {
+    private boolean versionError(String error) {
         if (!mFromAlarm) {
-            if (error != null) {
-                Utils.showToastOnUiThread(getContext(),
-                        getContext().getResources().getString(R.string.check_rom_updates_error)
-                                + ": " + error);
+            if (mCurrentServer < SERVERS.length - 1) {
+                nextServerCheck();
+                return true;
             } else {
-                Utils.showToastOnUiThread(getContext(), R.string.check_rom_updates_error);
+                if (error != null) {
+                    Utils.showToastOnUiThread(getContext(),
+                            getContext().getResources().getString(R.string.check_rom_updates_error)
+                                    + ": " + error);
+                } else {
+                    Utils.showToastOnUiThread(getContext(), R.string.check_rom_updates_error);
+                }
             }
         }
+        mCurrentServer = -1;
         fireCheckCompleted(null);
+        return false;
     }
 }

src/com/paranoid/paranoidota/updater/Server.java
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2013 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.paranoid.paranoidota.updater;
+
+import java.util.List;
+
+import com.paranoid.paranoidota.updater.Updater.PackageInfo;
+
+public interface Server {
+
+    public String getUrl(String device, double version);
+
+    public List<PackageInfo> createPackageInfoList(String buffer) throws Exception;
+
+    public String getError();
+}

src/com/paranoid/paranoidota/updater/server/GooServer.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright 2013 ParanoidAndroid Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.paranoid.paranoidota.updater.server;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.paranoid.paranoidota.updater.Server;
+import com.paranoid.paranoidota.updater.UpdatePackage;
+import com.paranoid.paranoidota.updater.Updater.PackageInfo;
+
+public class GooServer implements Server {
+
+    private static final String URL = "http://goo.im/json2&path=/devs/paranoidandroid/roms/%s&ro_board=%s";
+
+    private String mDevice = null;
+    private double mVersion = 0L;
+
+    @Override
+    public String getUrl(String device, double version) {
+        mDevice = device;
+        mVersion = version;
+        return String.format(URL, new Object[] { device, device });
+    }
+
+    @Override
+    public List<PackageInfo> createPackageInfoList(String buffer) throws Exception {
+        List<PackageInfo> list = new ArrayList<PackageInfo>();
+        if (buffer != null && !buffer.isEmpty()) {
+            JSONObject result = new JSONObject(buffer);
+            JSONObject update = null;
+            try {
+                update = result.getJSONObject("update_info");
+            } catch (JSONException ex) {
+                update = result;
+            }
+            JSONArray updates = update.optJSONArray("list");
+            for (int i = 0; updates != null && i < updates.length(); i++) {
+                JSONObject file = updates.getJSONObject(i);
+                String filename = file.optString("filename");
+                if (filename != null && !filename.isEmpty() && filename.endsWith(".zip")) {
+                    String stripped = filename.replaceAll(".1-RC1-", "-");
+                    stripped = stripped.replaceAll("-RC2-", "-");
+                    double version = Double.parseDouble(stripped.replaceAll("\\D+", ""));
+                    if (version > mVersion) {
+                        list.add(new UpdatePackage(mDevice, filename, version, "0", "http://goo.im"
+                                + file.getString("path"), file.getString("md5"), false));
+                    }
+                }
+            }
+        }
+        Collections.sort(list, new Comparator<PackageInfo>() {
+
+            @Override
+            public int compare(PackageInfo lhs, PackageInfo rhs) {
+                double v1 = lhs.getVersion();
+                double v2 = rhs.getVersion();
+                return v1 < v2 ? 1 : -1;
+            }
+
+        });
+        return list;
+    }
+
+    @Override
+    public String getError() {
+        return null;
+    }
+
+}

scotthartbti/android_packages_apps_BeanStalkOTA

Signed-off-by: scott <scott@ubuntu>
Conflicts:
	AndroidManifest.xml
	res/values/strings.xml
	src/com/beanstalk/beanstalkota/updater/GappsUpdater.java
	src/com/beanstalk/beanstalkota/updater/RomUpdater.java

src/com/beanstalk/beanstalkota/updater/RomUpdater.java
@@ -16,11 +16,7 @@
 
 package com.beanstalk.beanstalkota.updater;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import android.content.Context;
 
@@ -28,14 +24,21 @@ import com.beanstalk.beanstalkota.R;
 import com.beanstalk.beanstalkota.Utils;
 import com.beanstalk.beanstalkota.helpers.SettingsHelper;
 import com.beanstalk.beanstalkota.http.URLStringReader;
+import com.beanstalk.beanstalkota.updater.server.GooServer;
+import com.beanstalk.beanstalkota.updater.server.PaServer;
 
 public class RomUpdater extends Updater {
 
-    private static final String URL = "http://api.beanstalkandroid.co/updates/%s?v=%s";
+    private static final Server[] SERVERS = {
+        new PaServer(),
+        new GooServer()
+    };
 
     private SettingsHelper mSettingsHelper;
+    private Server mServer;
     private boolean mScanning = false;
     private boolean mFromAlarm;
+    private int mCurrentServer = -1;
 
     public RomUpdater(Context context, boolean fromAlarm) {
         super(context);
@@ -54,16 +57,23 @@ public class RomUpdater extends Updater {
         }
         mScanning = true;
         fireStartChecking();
-        long version = !Utils.weAreInAospa() ? 0L : getVersion();
-        new URLStringReader(this)
-                .execute(String.format(URL, new Object[] { getDevice(), version }));
+        nextServerCheck();
+    }
+
+    private void nextServerCheck() {
+        mScanning = true;
+        mCurrentServer++;
+        mServer = SERVERS[mCurrentServer];
+        new URLStringReader(this).execute(mServer.getUrl(getDevice(), getVersion()));
     }
 
     @Override
     public long getVersion() {
         String version = Utils.getProp(Utils.MOD_VERSION);
+        version = version.replaceAll(".1-RC1-", "-");
+        version = version.replaceAll("-RC2-", "-");
         String stripped = version.replaceAll("\\D+", "");
-        return Long.parseLong(stripped);
+        return "".equals(stripped) ? 0L : Long.parseLong(stripped);
     }
 
     @Override
@@ -86,22 +96,9 @@ public class RomUpdater extends Updater {
             mScanning = false;
             PackageInfo[] lastRoms = null;
             setLastUpdates(null);
-            String error = null;
-            List<PackageInfo> list = new ArrayList<PackageInfo>();
-            if (buffer != null && !buffer.isEmpty()) {
-                JSONObject updateInfo = new JSONObject(buffer);
-                error = updateInfo.optString("error");
-                if (error == null || error.isEmpty()) {
-                    JSONArray updates = updateInfo.getJSONArray("updates");
-                    for (int i = updates.length() - 1; i >= 0; i--) {
-                        JSONObject update = updates.getJSONObject(i);
-                        list.add(new UpdatePackage(getDevice(), update.getString("name"), update
-                                .getLong("version"), update.getString("size"), update
-                                .getString("url"), update.getString("md5"), false));
-                    }
-                }
-            }
+            List<PackageInfo> list = mServer.createPackageInfoList(buffer);
             lastRoms = list.toArray(new PackageInfo[list.size()]);
+            String error = mServer.getError();
             if (list.size() > 0) {
                 if (mFromAlarm) {
                     Utils.showNotification(getContext(), lastRoms, ROM_NOTIFICATION_ID, !Utils
@@ -110,13 +107,16 @@ public class RomUpdater extends Updater {
                 }
             } else {
                 if (error != null && !error.isEmpty()) {
-                    versionError(error);
+                    if (versionError(error)) {
+                        return;
+                    }
                 } else {
                     if (!mFromAlarm) {
                         Utils.showToastOnUiThread(getContext(), R.string.check_rom_updates_no_new);
                     }
                 }
             }
+            mCurrentServer = -1;
             setLastUpdates(lastRoms);
             fireCheckCompleted(lastRoms);
         } catch (Exception ex) {
@@ -132,16 +132,23 @@ public class RomUpdater extends Updater {
         versionError(null);
     }
 
-    private void versionError(String error) {
+    private boolean versionError(String error) {
         if (!mFromAlarm) {
-            if (error != null) {
-                Utils.showToastOnUiThread(getContext(),
-                        getContext().getResources().getString(R.string.check_rom_updates_error)
-                                + ": " + error);
+            if (mCurrentServer < SERVERS.length - 1) {
+                nextServerCheck();
+                return true;
             } else {
-                Utils.showToastOnUiThread(getContext(), R.string.check_rom_updates_error);
+                if (error != null) {
+                    Utils.showToastOnUiThread(getContext(),
+                            getContext().getResources().getString(R.string.check_rom_updates_error)
+                                    + ": " + error);
+                } else {
+                    Utils.showToastOnUiThread(getContext(), R.string.check_rom_updates_error);
+                }
             }
         }
+        mCurrentServer = -1;
         fireCheckCompleted(null);
+        return false;
     }
 }

src/com/beanstalk/beanstalkota/updater/Server.java
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2013 BeanStalk Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.beanstalk.beanstalkota.updater;
+
+import java.util.List;
+
+import com.beanstalk.beanstalkota.updater.Updater.PackageInfo;
+
+public interface Server {
+
+    public String getUrl(String device, double version);
+
+    public List<PackageInfo> createPackageInfoList(String buffer) throws Exception;
+
+    public String getError();
+}

src/com/beanstalk/beanstalkota/updater/server/GooServer.java
@@ -1,0 +1,90 @@
+/*
+ * Copyright 2013 BeanStalk Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.beanstalk.beanstalkota.updater.server;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.beanstalk.beanstalkota.updater.Server;
+import com.beanstalk.beanstalkota.updater.UpdatePackage;
+import com.beanstalk.beanstalkota.updater.Updater.PackageInfo;
+
+public class GooServer implements Server {
+
+    private static final String URL = "http://goo.im/json2&path=/devs/beanstalk/%s&ro_board=%s";
+
+    private String mDevice = null;
+    private double mVersion = 0L;
+
+    @Override
+    public String getUrl(String device, double version) {
+        mDevice = device;
+        mVersion = version;
+        return String.format(URL, new Object[] { device, device });
+    }
+
+    @Override
+    public List<PackageInfo> createPackageInfoList(String buffer) throws Exception {
+        List<PackageInfo> list = new ArrayList<PackageInfo>();
+        if (buffer != null && !buffer.isEmpty()) {
+            JSONObject result = new JSONObject(buffer);
+            JSONObject update = null;
+            try {
+                update = result.getJSONObject("update_info");
+            } catch (JSONException ex) {
+                update = result;
+            }
+            JSONArray updates = update.optJSONArray("list");
+            for (int i = 0; updates != null && i < updates.length(); i++) {
+                JSONObject file = updates.getJSONObject(i);
+                String filename = file.optString("filename");
+                if (filename != null && !filename.isEmpty() && filename.endsWith(".zip")) {
+                    String stripped = filename.replaceAll(".1-RC1-", "-");
+                    stripped = stripped.replaceAll("-RC2-", "-");
+                    double version = Double.parseDouble(stripped.replaceAll("\\D+", ""));
+                    if (version > mVersion) {
+                        list.add(new UpdatePackage(mDevice, filename, version, "0", "http://goo.im"
+                                + file.getString("path"), file.getString("md5"), false));
+                    }
+                }
+            }
+        }
+        Collections.sort(list, new Comparator<PackageInfo>() {
+
+            @Override
+            public int compare(PackageInfo lhs, PackageInfo rhs) {
+                double v1 = lhs.getVersion();
+                double v2 = rhs.getVersion();
+                return v1 < v2 ? 1 : -1;
+            }
+
+        });
+        return list;
+    }
+
+    @Override
+    public String getError() {
+        return null;
+    }
+
+}